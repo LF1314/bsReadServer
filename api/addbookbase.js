10d4cec552d599058ccfd15ca3d432e1222de7f7
LF134 <1279826137@qq.com>
2019-04-13 13:03:02 +0800

    修改添加书籍接口

--- api/addbookbase.js
+++ api/addbookbase.js
@@ -5,26 +5,28 @@ const addbaseBook = (req, res) => {
     const name = req.body.name,
      author = req.body.author,
      images = req.body.images,
-     ratings = req.body.ratings,
-     wordcount=req.body.wordcount,
+     ratings = parseFloat(req.body.ratings),
+     wordcount=parseFloat(req.body.wordcount),
      type=req.body.type,
      serialize=req.body.serialize,
      like =req.body.like,
      intro=req.body.intro
      
+    //  console.log(req.body)
 
     connect(`SELECT * FROM booklist WHERE name="${name}";`, function (err, results, fileds) {
       
         if (err) throw err;
         if (results.length >= 1) {
             res.send({
-                code: 200,
+                code: 400,
                 msg:"书籍已存在"
             })
         } else {
             let sql = `INSERT INTO \`books\`.\`booklist\`( \`name\`, \`author\`, \`images\`, \`ratings\`, \`wordcount\`, \`type\`, \`serialize\`, \`like\`, \`intro\`) 
-                       VALUES ('${name}', '${author}', '${images}', '${ratings}','${wordcount}''${type}''${serialize}''${like}''${intro}');`
+                       VALUES ('${name}', '${author}', '${images}', '${ratings}','${wordcount}','${type}','${serialize}','${like}','${intro}');`
             connect(sql, function (err, results, fileds) {
+                // console.log(err)
                 if (err) throw err;
                 if (results.affectedRows === 1) {
                      res.send({
